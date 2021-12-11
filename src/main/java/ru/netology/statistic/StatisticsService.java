73780a7540db49d96f14479d70b46f9a5c894e29
ElenaMughi <elena.mughi@gmail.com>
2021-12-11 15:28:41 +0300

    String Bag

---
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,25 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+    /**
+     * Calculate index of max income
+     *
+     * @param incomes - array of incomes
+     * @return - index of first max value
+     */
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+
+        String a = "aaa";
+        String b = "bbb";
+        if (a == b) return current_max_index;
+
+        return current_max;
+
+    }
+
+}
